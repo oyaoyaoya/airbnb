d852242ea864a0d33d6df47e0958f819d715f3fc
ryokotmng <ryokotmng@gmail.com>
2017-08-03 19:53:51 +0900

    deactivate turbolinks and html files amendments

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,12 +5,12 @@ class Room < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  validates :home_type, presents: true
-  validates :room_type, presents: true
-  validates :accomodate, presents: true
-  validates :bed_room, presents: true
-  validates :bath_room, presents: true
-  validates :listing_name, presents: true, length: {maximum: 50}
-  validates :summary, presents: true, length: {maximum: 500}
-  validates :address, presents: true
+  validates :home_type, presence: true
+  validates :room_type, presence: true
+  validates :accomodate, presence: true
+  validates :bed_room, presence: true
+  validates :bath_room, presence: true
+  validates :listing_name, presence: true, length: {maximum: 50}
+  validates :summary, presence: true, length: {maximum: 500}
+  validates :address, presence: true
 end
