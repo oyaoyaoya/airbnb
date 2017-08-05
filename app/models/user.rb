2ffcb6bf8e17dacec42c9d30f876ce0462222c64
ryokotmng <ryokotmng@gmail.com>
2017-08-05 17:48:33 +0900

    amendment views and etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,20 @@ validates :fullname, presence: true, length: {maximum: 50}
 has_many :rooms
 has_many :reservations
 
-def self.from_omniauth(auth)
-  user = User.where(email: auth.info.email).first
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
 
-  if user
-    return user
-  else
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.fullname = auth.info.name
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.image = auth.info.image
-      user.password = Devise.friendly_token[0,20]
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.fullname = auth.info.name
+        user.image = auth.info.image
+        user.uid = auth.uid
+        user.provider = auth.provider
+        user.skip_confirmation!
     end
   end
 end
