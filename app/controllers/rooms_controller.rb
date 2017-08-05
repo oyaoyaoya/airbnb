a7bd9d2ae16ff647f3181e984bf8c3097993d92d
ryokotmng <ryokotmng@gmail.com>
2017-08-05 20:56:34 +0900

    amendment for reservation form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
 before_action :set_room, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :is_authorised, only: [:listing, :pricing, :description, :photo_upload, :amenities, :location, :update]
 
   def index
     @rooms = current_user.rooms
