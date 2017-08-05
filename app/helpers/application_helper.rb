1e98d613723fff5b66b59050847415fcd1fc57ed
ryokotmng <ryokotmng@gmail.com>
2017-08-05 16:36:39 +0900

    amendment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     if user.image
-      user.image
+      "http://graph.facebook.com/#{user.uid}/picture?type=large"
     else
-    "https://s.gravatar.com/avatar/#{gravatar_id}?s=80"
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
     end
   end
 end
