73c80adcfa88c520e0b85255a8abd2d3dc76e4b6
ryokotmng <ryokotmng@gmail.com>
2017-08-05 16:21:52 +0900

    amendment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :email, :password])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description])
+  end
 end
+
