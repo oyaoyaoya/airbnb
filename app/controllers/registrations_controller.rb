c3eac7887603399577eb9bf5183a443e297bbdfc
ryokotmng <ryokotmng@gmail.com>
2017-08-10 21:43:44 +0900

    bundle install

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Devise::RegistrationsController < DeviseController
   protected
     def update_resource(resource, params)
-      resource.update_without_password(params)
+      resource.update_without_current_password(params)
     end
 end
 
