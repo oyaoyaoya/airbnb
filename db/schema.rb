e2f96c2d4f676c36bdec7c54d057b86c447cc87d
ryokotmng <ryokotmng@gmail.com>
2017-08-11 19:17:02 +0900

    remove scraping function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805133645) do
+ActiveRecord::Schema.define(version: 20170811071338) do
+
 
   create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "room_id"
