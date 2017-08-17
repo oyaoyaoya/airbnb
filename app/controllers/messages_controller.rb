4d59d51e5899b989272f59b47b8006d627b2cd44
ryokotmng <ryokotmng@gmail.com>
2017-08-17 19:31:49 +0900

    message controller index action created

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,12 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    if current_user == @conversation.sender || current_user == @conversation.recipient
+      @other = current_user == @conversation.sender ? @conversation.recipient : @conversation.sender
+      @messages = @conversation.messages.order("created_at DESC")
+    else
+      redirect_to conversations_path, alert: "You don't have permission to view this."
+    end
   end
 
   def create
