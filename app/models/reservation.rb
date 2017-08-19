29aa0915f008d9ab02e147180ac4ce4d768d3f7f
ryokotmng <ryokotmng@gmail.com>
2017-08-20 00:22:19 +0900

    reservation model amended

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,15 @@ class Reservation < ApplicationRecord
   enum status: {Waiting: 0, Approved: 1, Declined:2}
   belongs_to :user
   belongs_to :room
+
+  after_create_commit :create_notification
+  
+  private
+
+    def create_notification
+      type = self.room.Instant? ? "New Booking" : "New Request"
+      guest = User.find(self.user_id)
+
+      Notification.create(content: "#{type} from #{guest.fullname}", user_id: self.room.user_id)
+    end
 end
