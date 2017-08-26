2f41d2550a4639acffdd5be31b7ab12e97a6a66b
ryokotmng <ryokotmng@gmail.com>
2017-08-26 21:46:33 +0900

    views amended

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
-class Devise::RegistrationsController < DeviseController
+class RegistrationsController < Devise::RegistrationsController
   protected
     def update_resource(resource, params)
-      resource.update_without_current_password(params)
+      resource.update_without_password(params)
     end
 end
 
