463007e804fd3801eca7d6070911e130a5c0bf36
ryokotmng <ryokotmng@gmail.com>
2017-08-28 19:35:01 +0900

    test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,9 @@
 app_path = File.dirname(File.dirname(Dir.pwd))
-working_directory "#{app_path}/"
-pid "#{app_path}/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+puts app_path
+working_directory "#{app_path}/current"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
