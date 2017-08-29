42a4702531d73e1393cf6a030e24ffc0b1811387
ryokotmng <ryokotmng@gmail.com>
2017-08-29 11:02:59 +0900

    test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 puts "------------------------------------------------"
 puts app_path
 puts "------------------------------------------------"
