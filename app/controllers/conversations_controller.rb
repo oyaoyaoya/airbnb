5647b0902d3b1a26c118fa22a34b7bcd30d6e6c8
Hayato oyanagi <oyana@oya.local>
2017-09-01 22:42:00 +0900

    不要なインデントの除去、メソッドの分割

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,8 +7,8 @@ class ConversationsController < ApplicationController
 
   def create
 
-    if Conversation.where(sender_id: params[:sender_id]).where(recipient_id: params[:recipient_id]).present? || Conversation.where(sender_id: params[:recipient_id]).where(recipient_id: params[:sender_id]).present?
-
+    if Conversation.where(sender_id: params[:sender_id]).where(recipient_id: params[:recipient_id]).present?
+    || Conversation.where(sender_id: params[:recipient_id]).where(recipient_id: params[:sender_id]).present?
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
     else
       @conversation = Conversation.create(conversation_params)
@@ -19,7 +19,7 @@ class ConversationsController < ApplicationController
 
   private
 
-    def conversation_params
-      params.permit(:sender_id, :recipient_id)
-    end
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
 end
