52b6f8ae474b4a205e5154ddd5ca1238655caab0
Hayato oyanagi <oyana@oya.local>
2017-09-02 11:12:38 +0900

    変数の導入

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,15 @@ class PagesController < ApplicationController
   end
 
   def search
-    @rooms_address = Room.search_from_location(params[:search])
+    name, start_date, end_date = params[:search], params[:start_date], params[:end_date]
+
+    @rooms_address = Room.search_from_location(name)
     @search = @rooms_address.ransack(params[:q])
     @rooms = @search.result
     @arrRooms = @rooms.to_a
-
-    unless params[:start_date].blank? && params[:end_date].blank?
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
+    unless start_date.blank? && end_date.blank?
+      start_date = Date.parse(start_date)
+      end_date = Date.parse(end_date)
       @rooms.each do |room|
         not_available = room.reservations.where(
           "((? <= start_date AND start_date <= ?)
@@ -27,16 +28,14 @@ class PagesController < ApplicationController
           @arrRooms.delete(room)
         end
         # 今やっている事
-        # 1,
-        # 中身の入っている配列から、ダブっているのを削除
-        # 2, 23 ~ 32行をActiverecordのエリインターフェースを使って記述
+        # 1, 23 ~ 32行をActiverecordのエリインターフェースを使って記述
         # https://railsguides.jp/active_record_querying.html#or%E6%9D%A1%E4%BB%B6
         #  - 現状： SQLに近い記述
-        #  - ゴール： ActiveRecordのクエリインターフェースを使って記述
+        #  - ゴール： ActiveRecordのクエリインターフェースを使って記述（Railsのレールに乗る）
+        # 　　　　　　例：where / not / or メソッド当たり
         #      メリット: SQLはDBに依存しているため互換性が少なくなる
         #               (mysqlや、SQLiteの方言を使うと通じない)
         # 2, ドモルガンの法則で最適化できるかな?(個人的に楽しそう)
-        #
       end
     end
   end
